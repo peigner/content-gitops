cc5200ed06b95bbfa6be710a878aa08291fd81b4
peigner <109295289+peigner@users.noreply.github.com>
2022-09-06 17:00:06 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Partyyyy to the Peeopleee  dam dam dam dam dam uz uz uz"
+    return "P22artyyyy to the Peeopleee  dam dam dam dam dam uz uz uz"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
